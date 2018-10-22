bd3336aac56306a13788b1b5a15842276eb4854b
Francis Poirier <francispoirier05@hotmail.com>
2018-10-22 18:21:35 -0400

    LoginPageView entries change to red on errors

---
+++ Lab8_Validations/Lab8_Validations/Lab8_Validations/Converters/IsValueGreaterThanZeroConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using Xamarin.Forms;
+namespace Lab8_Validations.Converters
+{
+    public class IsValueGreaterThanZeroConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((int)value) > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
